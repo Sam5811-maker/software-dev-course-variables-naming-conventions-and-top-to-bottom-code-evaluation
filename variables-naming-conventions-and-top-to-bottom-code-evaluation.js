c95dad5ca1fdcfadc0ba73e8d12d02e4d03a088c
Shravan Samudrala <shravans5811@gmail.com>
2025-03-17 14:47:25 -0500

    Explanation and refactored code

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -30,3 +30,33 @@ let c = 20;
 let d = a + " bought " + b + " items for $" + c + ".";
 
 console.log(d);
+
+
+/*
+Explanation:
+
+In this code snippet the variable like a, b, c, d were used which are ambiguous variables to be used and will not explain the purpose
+This require the use of clear and descriptive variable names.
+
+Refactor the Code: */
+// Descriptive variables
+let customerName = "Alice";
+let numberOfItemsPurchased = 5;
+let totalCost = 20;
+
+//Additional variables:
+let purchaseDate = "04-17-2025";
+let thankYouMessage = "Thank you ! Please visit again!"
+
+let customerStatement = customerName + " bought " + numberOfItemsPurchased + " items for $" + totalCost + " on " + purchaseDate + " " +  thankYouMessage + ".";
+
+//output statements:
+console.log(customerStatement);
+
+/* Things to reflect on:
+  - Why is it important to use meaningful variable names?
+    --> To minimize confusion/abiguity and to if another developer who is is other than the actual coder or any one who is reading should understand it. 
+  - What are the common pitfalls to avoid when naming variables?
+    --> Vague naming, complex naming and abbreviated naming conventions to be avoided
+  - How do clear variable names benefit team collaboration? 
+    --> Ease of understanding for a new joinee, easy understanding/self explanatory. */
